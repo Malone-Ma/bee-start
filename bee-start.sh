9e5ecc803515adb28cbd1fde0c65bfddff12089d
Malone <malone@labyun.cn>
2021-05-17 18:21:47 +0800

    Update bee-start.sh

--- bee-start.sh
+++ bee-start.sh
@@ -5,4 +5,4 @@ cmd_bee=$"bee start --config /root/.beeconfig/bee-config-1.yaml"
 
 echo -e "start screen bee... \n"
 
-spawn screen -S ${screen_bee_name} ${cmd_bee}
+screen -S ${screen_bee_name} ${cmd_bee}
