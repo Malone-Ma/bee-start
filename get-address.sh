4ded8f8561bdd4c8a341893dc45c52116a791109
Malone <malone@labyun.cn>
2021-05-18 20:40:36 +0800

    Update get-address.sh

--- get-address.sh
+++ get-address.sh
@@ -3,11 +3,11 @@
 export TERM=vt100
 
 echo "start get address from clef..."
-
+add_name=$"add"
 cmd_add=$"echo '{\"id\":1,\"jsonrpc\":\"2.0\",\"method\":\"account_list\"}' | nc -U /var/lib/bee-clef/clef.ipc"
 
 /usr/bin/expect <<EOF
-spawn screen -S address ${cmd_add}
+spawn screen -S ${add_name} ${cmd_add}
 send "\x03"
 expect eof
 EOF
