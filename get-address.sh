e1132f583a7c4d1be397c30d7cd70f5648cbd633
Malone <malone@labyun.cn>
2021-05-18 19:43:30 +0800

    Update get-address.sh

--- get-address.sh
+++ get-address.sh
@@ -3,8 +3,14 @@
 export TERM=vt100
 
 echo "start get address from clef..."
-echo '{"id": 1, "jsonrpc": "2.0", "method": "account_list"}' | nc -U /var/lib/bee-clef/clef.ipc > /root/address.json
-kill -2
+
+cmd_add=$"echo '{\"id\": 1, \"jsonrpc\": \"2.0\", \"method\": \"account_list\"}' | nc -U /var/lib/bee-clef/clef.ipc > /root/address.json"
+
+/usr/bin/expect <<EOF
+spawn screen -S address ${cmd_add}
+send "\x03"
+expect eof
+EOF
 
 cut -c 36-77 /root/address.json > /root/bee.address
 
